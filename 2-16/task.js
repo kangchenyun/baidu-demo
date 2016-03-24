920a2610fdbf4495d16dce7683b73442a8d84928
Chenyun Kang <kangchenyun@gmail.com>
2016-03-24 21:42:19 +0800

    完成任务16

--- 2-16/task.js
+++ 2-16/task.js
@@ -13,14 +13,63 @@ var aqiData = {};
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
+    var city = document.getElementById("aqi-city-input").value;
+    city = city.replace(/^\s+|\s+$/g, "");
 
+    if (city.match(/\D/g)) {
+        var value = document.getElementById("aqi-value-input").value;
+        value = value.replace(/^\s+|\s+$/, "");
+
+        if (value.match(/\d/) && value.indexOf(".") == -1) {
+            if (value < 9999) {
+                aqiData[city] = value;
+            } else {
+                alert("空气质量指数需小于 9999");
+            }
+        } else {
+            alert("空气质量指数需为整数");
+        }
+
+    } else {
+        alert("城市名需为中英文字符");
+    }
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
+    var table = document.getElementById("aqi-table");
 
+    // 渲染数据时，清空表格之前先保留下表头部分
+    var remain_part = table.getElementsByTagName("tr")[0];
+    table.innerHTML = null;
+    table.appendChild(remain_part);
+
+    for (var i in aqiData) {
+
+        // 这个验证是因为删除时，删除了城市的空气指数
+        // 当空气指数为null时，不渲染该城市数据
+        if (aqiData[i]) {
+            var row = document.createElement("tr");
+            var city = document.createElement("td");
+            var value = document.createElement("td");
+            var button = document.createElement("button");
+            city.innerHTML = i;
+            value.innerHTML = aqiData[i];
+            button.innerHTML = "删除";
+            button.onclick = function () {
+
+                // 传递button元素，以便查找其对应的城市
+                delBtnHandle(this);
+            };
+
+            row.appendChild(city);
+            row.appendChild(value);
+            row.appendChild(button);
+            table.appendChild(row);
+        }
+    }
 }
 
 /**
@@ -36,8 +85,18 @@ function addBtnHandle() {
  * 点击各个删除按钮的时候的处理逻辑
  * 获取哪个城市数据被删，删除数据，更新表格显示
  */
-function delBtnHandle() {
+function delBtnHandle(button) {
     // do sth.
+
+    // 找到button的父节点，父节点的第一个子节点包含城市
+    // 如果该城市与集合中的一个城市名相同，则将空气指数设为null
+    var button_parent = button.parentNode;
+    var city = button_parent.firstChild.innerHTML;
+    for (var i in aqiData) {
+        if (i == city) {
+            aqiData[i] = null;
+        }
+    }
 
     renderAqiList();
 }
@@ -53,9 +112,11 @@ function init() {
     var del_btn = table.getElementsByTagName("button");
     for (var i = 0, len = del_btn.length; i < len; i++) {
         var obj = del_btn[i];
-        obj.onclick = delBtnHandle;
+        obj.onclick = function () {
+            delBtnHandle(this);
+        }
     }
 
 }
 
-init();
+window.onload = init;
